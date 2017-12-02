d8eb5b8121d8096ec223d3e153588b84409ba2ea
Rodrigo Donini <donini@gmail.com>
2017-12-01 23:07:46 -0200

    Minor changes

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-
 app.listen(port, (err) => {
   if (err) {
     return console.log('something bad happened', err)
